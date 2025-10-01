f66714e050c5b7f2ccafa87beecd8158efdd639f
Nick Wilma <nick.wilma24@googlemail.com>
2025-10-01 16:46:23 +0200

    feat: add cd command to terminal

--- src/programs/Terminal.jsx
+++ src/programs/Terminal.jsx
@@ -9,9 +9,10 @@ export default function Terminal() {
 
     const commands = {
         ls: commandLs,
+        cd: commandCd,
     }
 
-    const dir = "C:/aliens/mark"
+    const [dir, setDir] = useState("C:/aliens/mark");
 
     const [textRows, setTextRows] = useState(tempTextRows);
     const [inputVal, setInputVal] = useState("");
@@ -33,10 +34,11 @@ export default function Terminal() {
             const rows = [];
             rows.push(dir +" > " + inputVal);
 
-            const inputLower = inputVal.toLowerCase();
+            const inputLower = inputVal.toLowerCase().split(" ")[0];
+            const rest = inputVal.toLowerCase().split(" ").slice(1);
 
             if(commands.hasOwnProperty(inputLower)) {
-                rows.push(commands[inputLower](inputLower));
+                rows.push(commands[inputLower](rest));
             } else {
                 rows.push("command not found!");
             }
@@ -48,6 +50,14 @@ export default function Terminal() {
 
     function commandLs() {
         return "hallo, 123";
+    }
+
+    function commandCd(rest) {
+        if(rest.length <= 0) {
+            return "Error: no dir specified";
+        }
+
+        setDir(rest[0]);
     }
 
     return <div className="w-full h-full bg-slate-900 text-white p-2 flex flex-col">
