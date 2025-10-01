daa9bb7b218cc0182ee267cd0d297754a54677d6
Nick Wilma <nick.wilma24@googlemail.com>
2025-10-01 14:46:46 +0200

    fix: photos counter

--- src/programs/Photos.jsx
+++ src/programs/Photos.jsx
@@ -18,18 +18,15 @@ export default function Photos() {
         }
     ];
 
-    let counter = 0;
-    const [pic, setPic] = useState(pics[counter]);
+    const [counter, setCounter] = useState(0);
     const [showProps, setShowProps] = useState(false);
 
     function nextPic() {
-        counter = Math.min(counter + 1, pics.length-1);
-        setPic(pics[counter]);
+        setCounter(Math.min(counter + 1, pics.length-1));
     }
 
     function prevPic() {
-        counter = Math.max(counter - 1, 0);
-        setPic(pics[counter]);
+        setCounter(Math.max(counter - 1, 0));
     }
 
     function toggleProps() {
@@ -37,15 +34,15 @@ export default function Photos() {
     }
 
     return <div className="w-full h-full bg-slate-900 text-white p-2 flex flex-col gap-2">
-        <span className="w-full text-center">{pic.name + " (" + (counter+1) + "/" + (pics.length) + ")"}</span>
+        <span className="w-full text-center">{pics[counter].name + " (" + (counter+1) + "/" + (pics.length) + ")"}</span>
         <div className="w-full flex flex-row justify-center h-[300px]">
             {
                 showProps ? <div>{
-                    Object.keys(pic.props || {}).map(key => <span className="flex w-[300px]">
+                    Object.keys(pics[counter].props || {}).map(key => <span className="flex w-[300px]">
                         <div className="w-1/2">{key}</div>
-                        <div className="w-1/2">{pic.props[key]}</div>
+                        <div className="w-1/2">{pics[counter].props[key]}</div>
                     </span>)
-                }</div> : <img src={pic.url} alt={pic.name} className="w-[300px]"/>
+                }</div> : <img src={pics[counter].url} alt={pics[counter].name} className="w-[300px]"/>
             }
         </div>
         <div className="w-full flex flex-row gap-2">
